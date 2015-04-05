8eee7f7eca905e765bb451ace55f3331861a5be9
Iacopo Spalletti <i.spalletti@nephila.it>
2015-04-05 23:52:29 +0200

    Add rename tables for django CMS 3+

---
+++ djangocms_twitter/migrations/0004_rename_tables.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from distutils.version import LooseVersion
+
+import cms
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        if LooseVersion(cms.__version__) >= LooseVersion('3'):
+            db.rename_table(u'cmsplugin_twittersearch', u'djangocms_twitter_twittersearch')
+            db.rename_table(u'cmsplugin_twitterrecententries', u'djangocms_twitter_twitterrecententries')
+
+
+    def backwards(self, orm):
+
+        if LooseVersion(cms.__version__) >= LooseVersion('3'):
+            db.rename_table(u'djangocms_twitter_twittersearch', u'cmsplugin_twittersearch')
+            db.rename_table(u'djangocms_twitter_twitterrecententries', u'cmsplugin_twitterrecententries')
+
+    models = {
+        'cms.cmsplugin': {
+            'Meta': {'object_name': 'CMSPlugin'},
+            'changed_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'depth': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '15', 'db_index': 'True'}),
+            'numchild': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.CMSPlugin']", 'null': 'True', 'blank': 'True'}),
+            'path': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'placeholder': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Placeholder']", 'null': 'True'}),
+            'plugin_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
+            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'cms.placeholder': {
+            'Meta': {'object_name': 'Placeholder'},
+            'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slot': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'})
+        },
+        u'djangocms_twitter.twitterrecententries': {
+            'Meta': {'object_name': 'TwitterRecentEntries', '_ormbases': ['cms.CMSPlugin']},
+            u'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
+            'count': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '3'}),
+            'link_hint': ('django.db.models.fields.CharField', [], {'max_length': '75', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '75', 'blank': 'True'}),
+            'twitter_id': ('django.db.models.fields.CharField', [], {'max_length': '75'}),
+            'twitter_user': ('django.db.models.fields.CharField', [], {'max_length': '75'})
+        },
+        u'djangocms_twitter.twittersearch': {
+            'Meta': {'object_name': 'TwitterSearch', '_ormbases': ['cms.CMSPlugin']},
+            u'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
+            'count': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '3'}),
+            'query': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '200', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '75', 'blank': 'True'}),
+            'twitter_id': ('django.db.models.fields.CharField', [], {'max_length': '75'})
+        }
+    }
+
+    complete_apps = ['djangocms_twitter']
