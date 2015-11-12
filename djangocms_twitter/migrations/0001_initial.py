ba96b52841c90e80c5831dea69cff55f445e9941
Christian Strappazzon <christian.strappazzon@indicod-ecr.it>
2015-11-12 17:21:55 +0100

    Migrations moved to the standard location

--- djangocms_twitter/migrations/0001_initial.py
+++ djangocms_twitter/migrations/0001_initial.py
@@ -1,78 +1,43 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'TwitterRecentEntries'
-        db.create_table('cmsplugin_twitterrecententries', (
-            ('cmsplugin_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cms.CMSPlugin'], unique=True, primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=75, blank=True)),
-            ('twitter_user', self.gf('django.db.models.fields.CharField')(max_length=75)),
-            ('count', self.gf('django.db.models.fields.PositiveSmallIntegerField')(default=3)),
-            ('link_hint', self.gf('django.db.models.fields.CharField')(max_length=75, blank=True)),
-        ))
-        db.send_create_signal('djangocms_twitter', ['TwitterRecentEntries'])
+    dependencies = [
+        ('cms', '0013_auto_20151112_1654'),
+    ]
 
-        # Adding model 'TwitterSearch'
-        db.create_table('cmsplugin_twittersearch', (
-            ('cmsplugin_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cms.CMSPlugin'], unique=True, primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=75, blank=True)),
-            ('query', self.gf('django.db.models.fields.CharField')(default='', max_length=200, blank=True)),
-            ('count', self.gf('django.db.models.fields.PositiveSmallIntegerField')(default=3)),
-        ))
-        db.send_create_signal('djangocms_twitter', ['TwitterSearch'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'TwitterRecentEntries'
-        db.delete_table('cmsplugin_twitterrecententries')
-
-        # Deleting model 'TwitterSearch'
-        db.delete_table('cmsplugin_twittersearch')
-
-
-    models = {
-        'cms.cmsplugin': {
-            'Meta': {'object_name': 'CMSPlugin'},
-            'changed_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 6, 23, 0, 0)'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'language': ('django.db.models.fields.CharField', [], {'max_length': '15', 'db_index': 'True'}),
-            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.CMSPlugin']", 'null': 'True', 'blank': 'True'}),
-            'placeholder': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Placeholder']", 'null': 'True'}),
-            'plugin_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
-            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
-        },
-        'cms.placeholder': {
-            'Meta': {'object_name': 'Placeholder'},
-            'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'slot': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'})
-        },
-        'djangocms_twitter.twitterrecententries': {
-            'Meta': {'object_name': 'TwitterRecentEntries', 'db_table': "'cmsplugin_twitterrecententries'", '_ormbases': ['cms.CMSPlugin']},
-            'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
-            'count': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '3'}),
-            'link_hint': ('django.db.models.fields.CharField', [], {'max_length': '75', 'blank': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '75', 'blank': 'True'}),
-            'twitter_user': ('django.db.models.fields.CharField', [], {'max_length': '75'})
-        },
-        'djangocms_twitter.twittersearch': {
-            'Meta': {'object_name': 'TwitterSearch', 'db_table': "'cmsplugin_twittersearch'", '_ormbases': ['cms.CMSPlugin']},
-            'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
-            'count': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '3'}),
-            'query': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '200', 'blank': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '75', 'blank': 'True'})
-        }
-    }
-
-    complete_apps = ['djangocms_twitter']
+    operations = [
+        migrations.CreateModel(
+            name='TwitterRecentEntries',
+            fields=[
+                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
+                ('title', models.CharField(max_length=75, verbose_name='title', blank=True)),
+                ('twitter_user', models.CharField(max_length=75, verbose_name='twitter user')),
+                ('count', models.PositiveSmallIntegerField(default=3, help_text='Number of entries to display', verbose_name='count')),
+                ('link_hint', models.CharField(help_text='Deprecated: no longer used by Twitter widgets.', max_length=75, verbose_name='link hint', blank=True)),
+                ('twitter_id', models.CharField(help_text='See https://twitter.com/settings/widgets on how to obtain one', max_length=75, verbose_name='twitter id')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('cms.cmsplugin',),
+        ),
+        migrations.CreateModel(
+            name='TwitterSearch',
+            fields=[
+                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
+                ('title', models.CharField(max_length=75, verbose_name='title', blank=True)),
+                ('query', models.CharField(default=b'', help_text='Deprecated: no longer used by Twitter widgets. Define one when creating widgets.', max_length=200, verbose_name='query', blank=True)),
+                ('count', models.PositiveSmallIntegerField(default=3, help_text='Number of entries to display', verbose_name='count')),
+                ('twitter_id', models.CharField(help_text='See https://twitter.com/settings/widgets on how to obtain one', max_length=75, verbose_name='twitter id')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('cms.cmsplugin',),
+        ),
+    ]
